d738a27d8d6c9e019c259c1f822a17e2e3a7deea
Modest Industries <info@modestindustries.com>
2018-06-18 11:22:34 -0700

    Added rudimentary argument parsing, courtesy of Greg Neagle's code.

--- file_path_len.py
+++ file_path_len.py
@@ -2,30 +2,59 @@
 
 # Takes a directory as an option, recursively checks the directory structure for files with a pathname with more than 256 chars.
 
-import os, sys  
+import optparse
+import os
+import sys  
     
 def main():
-    # print command line arguments
-    #for arg in sys.argv[1:]:
-    #    print arg
+    '''Main'''
+    usage = "usage: %prog [options] /path/to/output.pkg"
         
     long_file_path = 0
     total_files = 0
+    
+    parser = optparse.OptionParser(usage=usage)
 
-    arg = sys.argv[1]   
-    os.chdir(arg)
-    print ("Starting in {}".format(arg))
+    optional_user_options = optparse.OptionGroup(
+        parser, 'Options')
+    optional_user_options.add_option(
+        '--characters', '-c',
+        help='Max file path characters. If this is not provided, the default of 256 is used.')
+    optional_user_options.add_option(
+        '--filepath', '-f', help='File path to start at. Defaults to the current directory.')
+
+    parser.add_option_group(optional_user_options)
+
+    options, arguments = parser.parse_args()
+
+    # verify options and arguments
+
+    print options
+
+    if options.characters:
+        max_len = options.characters
+    else:
+        max_len = 256
+        
+    if options.filepath:
+        start_location = options.filepath
+        os.chdir(start_location)
+    else:
+        start_location = os.getcwd()
+    
+    print ("Starting in {}".format(start_location))
+    print ("Looking for file paths longer than {} characters.".format(max_len))
 
     for dirpath, dirs, files in os.walk("."):
         path_len = len(dirpath)
         total_files += 1
-        if (path_len >= 256):
+        if (path_len >= max_len):
             print ("{}: {}".format(path_len, dirpath))
             long_file_path += 1
     
     # Tell me how many files were checked, and how many long file paths were found
     print ("Number of file paths checked: {}".format(total_files))
-    print ("Number of file paths with more than 256 characters : {}".format(long_file_path))
+    print ("Number of file paths with more than {} characters  : {}".format(max_len, long_file_path))
 
 if __name__ == "__main__":
     main()
